b67ca49ca1d74e950610087cd8e75c8e6a35b5d4
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-06-12 21:41:38 +0900

    es 2.3 support

--- src/main/java/org/docksidestage/esflute/maihama/cbean/bs/BsMenberCB.java
+++ src/main/java/org/docksidestage/esflute/maihama/cbean/bs/BsMenberCB.java
@@ -25,7 +25,6 @@ import org.docksidestage.esflute.maihama.cbean.MenberCB;
 import org.docksidestage.esflute.maihama.cbean.cq.MenberCQ;
 import org.docksidestage.esflute.maihama.cbean.cq.bs.BsMenberCQ;
 import org.dbflute.cbean.ConditionQuery;
-import org.elasticsearch.action.count.CountRequestBuilder;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 
@@ -81,16 +80,6 @@ public class BsMenberCB extends EsAbstractConditionBean {
     // ===================================================================================
     //                                                                               Build
     //                                                                               =====
-    @Override
-    public CountRequestBuilder build(CountRequestBuilder builder) {
-        if (_conditionQuery != null) {
-            QueryBuilder queryBuilder = _conditionQuery.getQuery();
-            if (queryBuilder != null) {
-                builder.setQuery(queryBuilder);
-            }
-        }
-        return builder;
-    }
 
     @Override
     public SearchRequestBuilder build(SearchRequestBuilder builder) {
