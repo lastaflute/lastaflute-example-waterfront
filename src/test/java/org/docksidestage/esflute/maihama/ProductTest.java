ef38967bf8a44d2f0c5d0e38f407278efc6633ef
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-10-30 23:11:44 +0900

    add base test class for product

---
+++ src/test/java/org/docksidestage/esflute/maihama/ProductTest.java
@@ -0,0 +1,79 @@
+package org.docksidestage.esflute.maihama;
+
+import static org.codelibs.elasticsearch.runner.ElasticsearchClusterRunner.newConfigs;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.codelibs.elasticsearch.runner.ElasticsearchClusterRunner;
+import org.docksidestage.esflute.maihama.exbhv.ProductBhv;
+import org.docksidestage.esflute.maihama.exentity.Product;
+import org.docksidestage.unit.UnitWaterfrontTestCase;
+import org.elasticsearch.common.settings.Settings.Builder;
+import org.lastaflute.di.exception.IORuntimeException;
+
+public class ProductTest extends UnitWaterfrontTestCase {
+
+    private ElasticsearchClusterRunner runner;
+
+    private String clusterName;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        clusterName = "es-waterfront-test";
+        // create runner instance
+        runner = new ElasticsearchClusterRunner();
+        // create ES nodes
+        runner.onBuild(new ElasticsearchClusterRunner.Builder() {
+            @Override
+            public void build(final int number, final Builder settingsBuilder) {
+                settingsBuilder.put("http.cors.enabled", true);
+                settingsBuilder.put("http.cors.allow-origin", "*");
+                settingsBuilder.putArray("discovery.zen.ping.unicast.hosts", "localhost:9301-9305");
+            }
+        }).build(newConfigs().clusterName(clusterName).numOfNode(3));
+
+        // wait for yellow status
+        runner.ensureYellow();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        // close runner
+        runner.close();
+        // delete all files
+        runner.clean();
+    }
+
+    public void test_runCluster() throws Exception {
+        final String index = "maihama";
+
+        // create an index
+        runner.createIndex(index, builder -> {
+            try (final InputStream input = Thread.currentThread().getContextClassLoader().getResource("create-maihama.json").openStream()) {
+                final String mappingSource = IOUtils.toString(input);
+                builder.setSource(mappingSource);
+            } catch (final IOException e) {
+                throw new IORuntimeException(e);
+            }
+            return builder;
+        });
+        runner.ensureYellow(index);
+
+        if (!runner.indexExists(index)) {
+            fail();
+        }
+
+        ProductBhv productBhv = getComponent(ProductBhv.class);
+
+        // insert
+        Product product = new Product();
+        product.setProductName("Test Product1");
+        productBhv.insert(product);
+
+        // wait for yellow status
+        runner.ensureYellow();
+    }
+}
